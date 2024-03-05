9961bbe0e38f11061f90e30f47793f5f514806c0
kotlecikmud <filippawlowski2012@gmail.com>
2024-03-05 12:41:08 +0100

    'bat_update'

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Chceck pyton version
+python --version
+REM Check for updates
+git pull
+REM Install/upgarde pip
+python.exe -m pip install --upgrade pip
+REM Install required libraries
+pip install -r requirements.txt
